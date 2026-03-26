205f44ffc54bf4695cc3a4508a7f348c137ed8cb
anhdungle93 <22119794+anhdungle93@users.noreply.github.com>
2026-03-26 17:07:46 +0700

    Initial commit: e-commerce SQLite query utilities

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { open } from "sqlite";
+import sqlite3 from "sqlite3";
+
+import { createSchema } from "./schema";
+import { getPendingOrders } from "./queries/order_queries";
+
+async function main() {
+  const db = await open({
+    filename: "ecommerce.db",
+    driver: sqlite3.Database,
+  });
+
+  await createSchema(db, false);
+
+  const pendingOrders = await getPendingOrders(db);
+  const staleOrders = pendingOrders.filter((o) => o.days_since_created > 3);
+  console.log("Orders pending longer than 3 days:", staleOrders);
+}
+
+main();
